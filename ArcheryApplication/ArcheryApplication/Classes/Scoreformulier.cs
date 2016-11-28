03f02d8cc7c265cc8b3c62759d1c2009f47492ac
Jelle Schrader <j.schrader@student.fontys.nl>
2016-11-28 09:43:47 +0100

    Scoreformulier uitbreiden, scores moeten anders worden aangemaakt

--- ArcheryApplication/ArcheryApplication/Classes/Scoreformulier.cs
+++ ArcheryApplication/ArcheryApplication/Classes/Scoreformulier.cs
@@ -9,5 +9,30 @@ namespace ArcheryApplication.Classes
     public class Scoreformulier
     {
         //al het gereken met scores komt hier
+        public List<Score> Score_36_pijlen { get; set; }
+        public List<Score> Score_30_pijlen { get; set; }
+        public List<Score> Score_25_pijlen { get; set; }
+        public List<Score> AnyPijlen { get; set; }
+
+        //constructor voor 36 pijlen
+        public Scoreformulier(int score)
+        {
+
+        }
+        //constructor voor 30 pijlen
+        public Scoreformulier(int score)
+        {
+
+        }
+        //constructor voor 25 pijlen
+        public Scoreformulier(int score)
+        {
+
+        }
+        //constructor voor een andere aantal pijlen
+        public Scoreformulier(int score)
+        {
+
+        }
     }
 }
